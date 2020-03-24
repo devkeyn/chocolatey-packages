2fdc8d3adb95e165b0573f723cac51782e2d6fc5
Keyn <devkeyn@gmail.com>
2020-03-24 08:00:20 +0200

    Initial

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,37 @@
+﻿
+$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://zello.com/data/ZelloSetup.exe'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe'
+  url           = $url
+  url64bit      = $url64
+  softwareName  = 'zello*'
+  checksum      = 'f646d5bdbe19638ed8fde0e2e8425bbc7f207732e64d308019ddab6e4bbfd44b'
+  checksumType  = 'sha256'
+  validExitCodes= @(0)
+  silentArgs   = '/S'
+}
+
+Install-ChocolateyPackage @packageArgs
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
